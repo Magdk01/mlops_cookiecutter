9ed8a146621f0479e45ef0943e9dc0d04ac4e6d8
Magdk01 <Magnusvinjebo@hotmail.com>
2024-01-08 11:45:55 +0100

    new action setup

--- mlops_cookiecutter/__init__.py
+++ mlops_cookiecutter/__init__.py
@@ -1,4 +1,5 @@
-from mlops_cookiecutter.models.model import MyAwesomeModel
+# from mlops_cookiecutter.models.model import MyAwesomeModel
 from mlops_cookiecutter.predict_model import predict
+from mlops_cookiecutter.models.model_lightning import MyAwesomeModel
 
 # from data.mnist_dataloader import mnist
